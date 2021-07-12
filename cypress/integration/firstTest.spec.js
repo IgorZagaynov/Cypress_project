d8c42e30de520dcce9f9c50b49af82c79b5fd622
igor zagainov <zagaynov@rb.ru>
2021-07-12 20:53:05 +0700

    lecture about finding elements by Get Find Eq

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,32 @@
+// describe("Mobile phone replenishment", () => {
+
+//     context("Replenishment less then the allowed around", () => {
+//         it("show error: Minimum amount of the replenishment 1 UAH", () => {});
+//     });
+
+// });
+
+it('By ID', () => {
+    cy.visit("http://www.facebook.com/")
+    cy.get('#email')
+});
+
+it('By Class', () => {
+    cy.visit("https://docs.cypress.io/api/commands/get#Syntax")
+    cy.get('.ds-input')
+});
+
+it('By Tag', () => {
+    cy.visit("https://docs.cypress.io/api/commands/get#Syntax")
+    cy.get('nav')
+});
+
+it('By Tag value', () => {
+    cy.visit("http://www.facebook.com/")
+    cy.get('[name="pass"]')
+});
+
+it.only('By different Tags', () => {
+    cy.visit("http://www.facebook.com/")
+    cy.get('[data-testid=open-registration-form-button][role="button"]')
+});
